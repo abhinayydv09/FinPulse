c1868398964187b6896f58f324ff3c29554fff1d
Abhinay Kumar Yadav <abhinayyadav9402@gmail.com>
2025-11-07 05:52:45 +0530

    Change log file handler mode from append to write

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s]: %(message)s",
     handlers=[
-        logging.FileHandler("logs/app.log", mode="a", encoding="utf-8"),
+        logging.FileHandler("logs/app.log", mode="w", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
